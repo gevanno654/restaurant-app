380b1417fd5e37e7495d71894c5b1a81fd9bad0e
gevanno654 <67539397+gevanno654@users.noreply.github.com>
2024-07-11 19:02:43 +0700

    Update access to database.

--- config/db.php
+++ config/db.php
@@ -1,8 +1,8 @@
 <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "restoran";
+    $servername = "gevannoyoh.com";
+    $username = "gevannoy_admin";
+    $password = "Gevanno060504";
+    $dbname = "gevannoy_restoran";
 
     // Create connection
     $conn = new mysqli($servername, $username, $password, $dbname);
